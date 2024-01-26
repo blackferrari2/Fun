78e48e0b7ece6613a02472deec992df3bc78b097
blackferrari2 <87984333+blackferrari2@users.noreply.github.com>
2024-01-26 20:49:28 -0300

    add shortcuts, add fixedupdates

---
+++ Packages/FixedUpdate.lua
@@ -0,0 +1,3 @@
+local REQUIRED_MODULE = require(script.Parent._Index["blackferrari2_fixedupdate@0.1.2"]["fixedupdate"])
+export type FixedUpdate = REQUIRED_MODULE.FixedUpdate 
+return REQUIRED_MODULE
